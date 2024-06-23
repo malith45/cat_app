33a950c53cd629e583c25c8c42de4db643831824
Malith Kumaranayaka <127102703+malith45@users.noreply.github.com>
2024-06-23 23:43:12 +0530

    appBar

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+              child: Text('Death awaits you...',
+                  style: TextStyle(color: Colors.white))),
+          backgroundColor: Colors.blueGrey[700],
+        ),
+      ),
+    ),
+  );
+}
